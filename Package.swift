8c2f4dd5a97e2c0c5750123424c24fb0fa1c709a
arror <arror@users.noreply.github.com>
2023-09-18 06:07:54 +0000

    AListKit Release 0.0.20!!!

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "AListKit",
-      url: "https://github.com/arror/AListKit/releases/download/0.0.19/AListKit.xcframework.zip",
-      checksum: "195499c4bf8ae999d13cb41042f694b3eb30e69efb745e51bbc05f4311cae97e"
+      url: "https://github.com/arror/AListKit/releases/download/0.0.20/AListKit.xcframework.zip",
+      checksum: "4bfe6e15e103e146a41cd8215f2940396caaf95e45bfceb3cfeb08296fc1f972"
     )
   ]
 )
