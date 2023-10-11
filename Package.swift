4ee6380bf601b6cb62d37b19c3c4429922c406a6
arror <arror@users.noreply.github.com>
2023-10-11 06:07:07 +0000

    AListKit Release 0.0.37!!!

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
   name: "AListKit",
-  platforms: [.iOS(.v15)],
+  platforms: [.iOS(.v12)],
   products: [
     .library(name: "AListKit", targets: ["AListKit"])
   ],
   targets: [
     .binaryTarget(
       name: "AListKit",
-      url: "https://github.com/arror/AListKit/releases/download/0.0.36/AListKit.xcframework.zip",
-      checksum: "5388afd9d4a3924ad2da1671433ff23dd924dfbd45c64f74508f47240500da88"
+      url: "https://github.com/arror/AListKit/releases/download/0.0.37/AListKit.xcframework.zip",
+      checksum: "6e664fbdd5eb7d50b1914ce924f99f39196fb08683b2cfe9e8f773aba9d53758"
     )
   ]
 )
